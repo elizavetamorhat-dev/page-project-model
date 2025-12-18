2b8cada39e254bd99f69c6e2068c85e6e69eb5c3
elizavetamorhat-dev <elizavetamorhat@gmail.com>
2025-12-18 20:31:03 +0200

    feat:added tests

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,12 +1,38 @@
-import { Locator, Page } from '@playwright/test'
+import { expect, Page } from '@playwright/test'
+import type { Locator } from '@playwright/test'
 
 export class OrderPage {
   readonly page: Page
   readonly statusButton: Locator
-  // add more locators here
+  readonly nameField: Locator
+  readonly phoneField: Locator
+  readonly commentField: Locator
+  readonly createOrderButton: Locator
+  readonly successfulCreationPopup: Locator
+  readonly logoutButton: Locator
 
   constructor(page: Page) {
     this.page = page
     this.statusButton = page.getByTestId('openStatusPopup-button')
+    this.nameField = page.getByTestId('username-input')
+    this.phoneField = page.getByTestId('phone-input')
+    this.commentField = page.getByTestId('comment-input')
+    this.createOrderButton = page.getByTestId('createOrder-button')
+    this.successfulCreationPopup = page.locator('main > .popup')
+    this.logoutButton = page.getByTestId('logout-button')
+  }
+
+  async checkInnerComponentsVisible(): Promise<void> {
+    await expect(this.statusButton).toBeVisible()
+    await expect(this.statusButton).toBeEnabled()
+    await expect(this.nameField).toBeVisible()
+    await expect(this.phoneField).toBeVisible()
+    await expect(this.commentField).toBeVisible()
+    await expect(this.createOrderButton).toBeVisible()
+    await expect(this.logoutButton).toBeVisible()
+  }
+
+  async checkCreationPopupVisible(visible = true): Promise<void> {
+    await expect(await this.successfulCreationPopup).toBeVisible({ visible })
   }
 }
--- tests/ui/e2e-flow-with-before.spec.ts
+++ tests/ui/e2e-flow-with-before.spec.ts
@@ -3,29 +3,41 @@ import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
-let loginPage: LoginPage
+let authPage: LoginPage
 
 test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page)
-  await loginPage.open()
+  authPage = new LoginPage(page)
+  await authPage.open()
 })
 
-test('signIn button disabled when incorrect data inserted', async ({}) => {
-  await loginPage.usernameField.fill(faker.lorem.word(2))
-  await loginPage.passwordField.fill(faker.lorem.word(7))
-  await expect(loginPage.signInButton).toBeDisabled()
+test('SignIn button disabled when incorrect data inserted', async ({}) => {
+  await authPage.usernameField.fill(faker.lorem.word(2))
+  await authPage.passwordField.fill(faker.lorem.word(7))
+  await expect(authPage.signInButton).toBeDisabled()
 })
 
-test('error message displayed when incorrect credentials used', async ({}) => {
+test.skip('Error message displayed when incorrect credentials used', async ({}) => {
   // implement test
 })
 
-test('login with correct credentials and verify order creation page', async ({}) => {
-  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
+test('Login with correct credentials and verify order creation page', async ({}) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await expect(orderCreationPage.statusButton).toBeVisible()
-  // verify at least few elements on the order creation page
+  await orderCreationPage.checkInnerComponentsVisible()
 })
 
-test('login and create order', async ({}) => {
-  // implement test
+test('Login and create order', async ({}) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.nameField.fill('test')
+  await orderCreationPage.phoneField.fill('test1234')
+  await orderCreationPage.commentField.fill('1234123')
+  await orderCreationPage.createOrderButton.click()
+  await orderCreationPage.checkCreationPopupVisible(true)
+})
+
+test('Logout', async () => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.checkInnerComponentsVisible()
+  await orderCreationPage.logoutButton.click()
+  await expect(authPage.signInButton).toBeVisible()
 })
